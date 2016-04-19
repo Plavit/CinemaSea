edb0075d96c8699d249bd4e03f9a4df594b84f36
David Löffler <loffldav@fel.cvut.cz>
2016-04-19 14:48:00 +0200

    conection to server updated

--- Program/src/main/java/Main/Database.java
+++ Program/src/main/java/Main/Database.java
@@ -13,42 +13,39 @@ import java.sql.*;
  * @author leffl_000
  */
 public class Database {
+    
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:postgresql://slon.felk.cvut.cz";
    
    static final String USER = "db16_loffldav";
    static final String PASS = "db16_loffldav";
+   
+   private User user;
 
     public Database() {
+    }
+    
+    public Database(User user){
+        this.user = user;
     }
    
    
    
    public void ConnectToServer(){
        Connection conn = null;
-       Statement stmt = null;
        
        try{
-           //Class.forName("com.mysql.jdbc.Driver");
            
            System.out.println("Connecting to database...");
            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-           System.out.println("CONNECTED !!!!!!!!!!!!!!");
+           System.out.println("CONNECTION SUCCESFUL");
            
-           //stmt.close();
            conn.close();
        }
        catch(Exception e){
            System.out.println("CONNECTION FAILED");
            System.out.println(e.toString());
-       }finally {
-           //finally block used to close resources
-           try {
-               if (stmt != null) {
-                   stmt.close();
-               }
-           } catch (SQLException se2) {
-           }// nothing we can do
+       }finally {           
            try {
                if (conn != null) {
                    conn.close();
