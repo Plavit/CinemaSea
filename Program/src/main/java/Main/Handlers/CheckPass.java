8f57465f3afc947f2503200a87c1cc1ecac7eead
Marek Szeles <szelemar@fel.cvut.cz>
2016-05-07 22:27:30 +0200

    cleanup

--- Program/src/main/java/Main/Handlers/CheckPass.java
+++ Program/src/main/java/Main/Handlers/CheckPass.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Handlers;
+package Main.Handlers;
 
 /**
  *
- * @authors Marek Szeles, David Loffler
+ * @author Marek
  */
-public class CheckPassEqual {
+public class CheckPass {
     
 }
